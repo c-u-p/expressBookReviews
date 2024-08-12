c6fa3fa5e70649377854406e958a73416ff153f4
c-u-p <8umeshpadul6@gmail.com>
2024-08-12 05:33:03 -0400

    Added till Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,30 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const keys = Object.keys(books);
+  let books_with_author = [];
+  keys.forEach(key => {
+    if(books[key]["author"] === author){
+        books_with_author.push(books[key]);
+    }
+  });
+  //const books_with_author= books.filter((book) => book.author === author);
+  //console.log(books_with_author);
+  return res.send(books_with_author);
 });
 
 // Get all books based on title
