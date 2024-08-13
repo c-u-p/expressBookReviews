d099287739b5bfb8d358f113e9b6ebd4ef92ca0e
c-u-p <8umeshpadul6@gmail.com>
2024-08-13 13:41:23 -0400

    Completed All the tasks of the project and Tested Successfully!

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,51 +38,71 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async (req, res) => {
   //Write your code here
-  return res.send(JSON.stringify(books, null, 4));
+  try{
+    return res.send(JSON.stringify(books, null, 4));
+  } catch(error) {
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async (req, res) => {
   //Write your code here
-  const isbn = req.params.isbn;
-  return res.send(books[isbn]);
+  try{
+    const isbn = req.params.isbn;
+    return res.send(books[isbn]);
+  } catch(error) {
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async (req, res) => {
   //Write your code here
-  const author = req.params.author;
-  const keys = Object.keys(books);
-  let books_with_author = [];
-  keys.forEach(key => {
-    if(books[key]["author"] === author){
-        books_with_author.push(books[key]);
-    }
-  });
-  return res.send(books_with_author);
+  try{
+    const author = req.params.author;
+    const keys = Object.keys(books);
+    let books_with_author = [];
+    keys.forEach(key => {
+        if(books[key]["author"] === author){
+            books_with_author.push(books[key]);
+        }
+    });
+    return res.send(books_with_author);
+  } catch(error) {
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async (req, res) => {
   //Write your code here
-  const title = req.params.title;
-  const keys = Object.keys(books);
-  let books_with_title = [];
-  keys.forEach(key => {
-    if(books[key]["title"] === title){
-        books_with_title.push(books[key]);
-    }
-  });
-  return res.send(books_with_title);
+  try{
+    const title = req.params.title;
+    const keys = Object.keys(books);
+    let books_with_title = [];
+    keys.forEach(key => {
+        if(books[key]["title"] === title){
+            books_with_title.push(books[key]);
+        }
+    });
+    return res.send(books_with_title);
+  } catch(error) {
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', async (req, res) => {
   //Write your code here
-  const isbn = req.params.isbn;
-  return res.send(books[isbn]["reviews"]);
+  try{
+    const isbn = req.params.isbn;
+    return res.send(books[isbn]["reviews"]);
+  } catch(error) {
+    return res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 module.exports.general = public_users;
